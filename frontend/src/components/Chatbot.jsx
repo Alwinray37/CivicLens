b4f82be002e021791802b54f50847dd2e5aa115d
Thomas Scott <t.scott12331@gmail.com>
2025-09-17 15:15:39 -0700

    Add scroll to agenda and transcript card components

--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -12,11 +12,35 @@ export default function Chatbot() {
         {
             type: "incoming",
             message: "The purpose of this meeting was to...",
-        }
+        },
+        {
+            type: "outgoing",
+            message: "What was the main purpose of this meeting?",
+        },
+        {
+            type: "incoming",
+            message: "The purpose of this meeting was to...",
+        },
+        {
+            type: "outgoing",
+            message: "What was the main purpose of this meeting?",
+        },
+        {
+            type: "incoming",
+            message: "The purpose of this meeting was to...",
+        },
+        {
+            type: "outgoing",
+            message: "What was the main purpose of this meeting?",
+        },
+        {
+            type: "incoming",
+            message: "The purpose of this meeting was to...",
+        },
     ]);
 
     return (
-        <div className={"border text-start p-2 h-100 d-flex flex-column justify-content-between "
+        <div className={"container border text-start p-2 h-100 d-flex flex-column justify-content-between "
                         + styles.chatbotWrapper}>
             <h4 className="border-bottom m-0 ps-1 pb-2">Chatbot</h4>
             <div className="d-flex flex-column flex-grow-1 overflow-scroll">
--- frontend/src/components/TranscriptCard.jsx
+++ frontend/src/components/TranscriptCard.jsx
@@ -4,9 +4,10 @@ export default function TranscriptCard({
     snippets,
 }) {
     return (
-        <div className="container border p-2">
+        <div className={"container border p-2 d-flex flex-column "
+                        + styles.transcriptCard}>
             <h4 className="m-0 pb-2 border-bottom ps-1 text-start">Transcript</h4>
-            <div className="overflow-scroll pt-2 d-flex flex-column gap-1">
+            <div className="overflow-scroll pt-2 d-flex flex-column gap-1 flex-grow-1">
                 {snippets.map((s, i) => 
                     <div className="border rounded rounded-2 p-3 d-flex justify-content-between"
                         key={i}
