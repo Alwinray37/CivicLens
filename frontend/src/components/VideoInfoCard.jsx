73ce72212579500228a74c79b2802e7d6612bd85
Thomas Scott <t.scott12331@gmail.com>
2025-11-08 03:04:31 -0800

    Fix attribute naming in VideoInfoCard

--- frontend/src/components/VideoInfoCard.jsx
+++ frontend/src/components/VideoInfoCard.jsx
@@ -10,25 +10,25 @@ export default function VideoInfoCard({
 }) {
     return (
         <div className="bg-body-secondary rounded p-0 shadow-sm ">
-            <ul class="nav nav-pills bg-body-tertiary p-2 rounded-top d-flex justify-content-between" id="video-info-tab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="video-summary-tab" data-bs-toggle="pill" data-bs-target="#video-summary" type="button" role="tab" aria-controls="video-summary" aria-selected="true">Summary</button>
+            <ul className="nav nav-pills bg-body-tertiary p-2 rounded-top d-flex justify-content-center gap-3 " id="video-info-tab" role="tablist">
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link active" id="video-summary-tab" data-bs-toggle="pill" data-bs-target="#video-summary" type="button" role="tab" aria-controls="video-summary" aria-selected="true">Summary</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="video-agenda-tab" data-bs-toggle="pill" data-bs-target="#video-agenda" type="button" role="tab" aria-controls="video-agenda" aria-selected="false">Agenda</button>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="video-agenda-tab" data-bs-toggle="pill" data-bs-target="#video-agenda" type="button" role="tab" aria-controls="video-agenda" aria-selected="false">Agenda</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="video-timeline-tab" data-bs-toggle="pill" data-bs-target="#video-timeline" type="button" role="tab" aria-controls="video-timeline" aria-selected="false">Timeline</button>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="video-timeline-tab" data-bs-toggle="pill" data-bs-target="#video-timeline" type="button" role="tab" aria-controls="video-timeline" aria-selected="false">Timeline</button>
                 </li>
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="video-transcript-tab" data-bs-toggle="pill" data-bs-target="#video-transcript" type="button" role="tab" aria-controls="video-transcript" aria-selected="false" >Transcript</button>
+                <li className="nav-item" role="presentation">
+                    <button className="nav-link" id="video-transcript-tab" data-bs-toggle="pill" data-bs-target="#video-transcript" type="button" role="tab" aria-controls="video-transcript" aria-selected="false" >Transcript</button>
                 </li>
             </ul>
-            <div class="tab-content overflow-hidden rounded-bottom" id="video-tabContent">
-                <div class="tab-pane fade show active" id="video-summary" role="tabpanel" aria-labelledby="video-summary-tab" tabindex="0">
+            <div className="tab-content overflow-hidden rounded-bottom" id="video-tabContent">
+                <div className="tab-pane fade show active" id="video-summary" role="tabpanel" aria-labelledby="video-summary-tab" tabIndex="0">
                     <SummaryCard />
                 </div>
-                <div class="tab-pane fade" id="video-agenda" role="tabpanel" aria-labelledby="video-agenda-tab" tabindex="0">
+                <div className="tab-pane fade" id="video-agenda" role="tabpanel" aria-labelledby="video-agenda-tab" tabIndex="0">
                      <AgendaCard 
                          events={
                              videoData.agenda.map(a => ({
@@ -41,10 +41,10 @@ export default function VideoInfoCard({
                      onItemClick={onTimeSelect}
                      />
                 </div>
-                <div class="tab-pane fade" id="video-timeline" role="tabpanel" aria-labelledby="video-timeline-tab" tabindex="0">
+                <div className="tab-pane fade" id="video-timeline" role="tabpanel" aria-labelledby="video-timeline-tab" tabIndex="0">
                     <TimelineCard />
                 </div>
-                <div class="tab-pane fade" id="video-transcript" role="tabpanel" aria-labelledby="video-transcript-tab" tabindex="0">
+                <div className="tab-pane fade" id="video-transcript" role="tabpanel" aria-labelledby="video-transcript-tab" tabIndex="0">
                      <TranscriptCard 
                          onItemClick={onTimeSelect}
                          snippets={[
